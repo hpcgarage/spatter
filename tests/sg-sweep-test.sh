96e39f622a1ec19f5ace63b8fde1c7f9979b9d03
Jeffrey Scott Young <jeffyoung3131@gmail.com>
2018-06-12 16:19:47 -0400

    Added some placeholders for tests and plotting

--- tests/sg-sweep-test.sh
+++ tests/sg-sweep-test.sh
@@ -5,3 +5,41 @@
 
 #Output file
 OUTPUT=$1
+
+#Block sizes to sweep
+BLKSIZE (1 2 4 8 16 32)
+#source length
+
+#dest length
+
+#index length
+
+#op - accumulate versus standard
+OP
+
+#kernel = scatter/gather/s+g
+
+for BLKSIZE
+do
+
+for SRCSIZE
+do
+
+for KERNEL
+do
+
+for OPTYPE
+do
+
+done
+
+
+
+done
+
+done
+
+done
+
+#execute the test script with nested for loops
+./test-ocl.sh <all of these parameters>
