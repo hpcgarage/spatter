af3f203877cce564607124754401915bd0d14b59
Patrick Lavin <plavin3@gatech.edu>
2018-07-30 13:10:44 -0400

    add gather tests

---
+++ tests/gather2.sh
@@ -0,0 +1,1 @@
+CL_HELPER_NO_COMPILER_OUTPUT_NAG=1 ./sgbench --backend=opencl --source-len=32 --target-len=16 --index-len=16 --kernel-file=kernels/gather2.cl --kernel-name=gather --cl-platform=nvidia --cl-device=titan --validate --block-len=20 --vector-len=2
---
+++ tests/gather8.sh
@@ -0,0 +1,1 @@
+CL_HELPER_NO_COMPILER_OUTPUT_NAG=1 ./sgbench --backend=opencl --source-len=256 --target-len=256 --index-len=256 --kernel-file=kernels/gather8.cl --kernel-name=gather --cl-platform=nvidia --cl-device=titan --validate --block-len=1 --vector-len=8
