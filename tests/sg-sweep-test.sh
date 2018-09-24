dc6bb2eb15684b3e71a261be1f38a2b8d9fa4585
Jeffrey Scott Young <jeffyoung3131@gmail.com>
2018-09-24 16:59:49 -0400

    Updated sweep script to allow for specifying output and different OpenCL kernels

--- tests/sg-sweep-test.sh
+++ tests/sg-sweep-test.sh
@@ -5,8 +5,8 @@
 #Print out each command for debugging purposes
 set -x
 
-#Output file
-OUTPUT=$1
+#User specified identifier to clarify the output
+DEVICE_NAME=$1
 
 BLKSIZE="1 2 4 8 16 32"
 #source length
@@ -16,7 +16,7 @@ SPARSITY="1 2 3 4 6 8 12 16 24 32 48 64 96 128"
 #Specify the vector length - OpenCL allows for specifying double16 for example
 VECTOR="1 2 4 8 16"
 #Specify the backend - openmp, cuda, opencl
-BACKEND=cuda
+BACKEND=$2
 #The number of iterations to run
 NUM_RUNS=10
 
@@ -33,12 +33,14 @@ do
 	#Specify the target length to scatter to or gather into
 	DST_LEN=$(($IDX_LEN*S))
 
+	OUTPUT_FILE=sg_${BACKEND}_${DEVICE_NAME}.ssv
+
 	#Special handling for the OpenCL case
-	if [ "$BACKEND" -eq "opencl" ]
+	if [ "${BACKEND}" == "opencl" ]
 	then
-   	    CL_HELPER_NO_COMPILER_OUTPUT_NAG=1 ./sgbench --backend=$BACKEND --source-len=$SRC_LEN --target-len=$DST_LEN --index-len=$IDX_LEN --kernel-name=scatter --cl-platform=nvidia --cl-device=titan --runs=$NUM_RUNS --vector-len=$V
+   	    CL_HELPER_NO_COMPILER_OUTPUT_NAG=1 ./sgbench --backend=$BACKEND --source-len=$SRC_LEN --target-len=$DST_LEN --index-len=$IDX_LEN -kernel-file=kernels/scatter${V}.cl --kernel-name=scatter --cl-platform=nvidia --cl-device=titan --runs=$NUM_RUNS --vector-len=$V &>> $OUTPUT_FILE
         else
-   	    ./sgbench --backend=$BACKEND --source-len=$SRC_LEN --target-len=$DST_LEN --index-len=$IDX_LEN --kernel-name=scatter --runs=$NUM_RUNS --vector-len=$V
+   	    ./sgbench --backend=$BACKEND --source-len=$SRC_LEN --target-len=$DST_LEN --index-len=$IDX_LEN --kernel-name=scatter --runs=$NUM_RUNS --vector-len=$V &>> $OUTPUT_FILE
         fi
    done
 
