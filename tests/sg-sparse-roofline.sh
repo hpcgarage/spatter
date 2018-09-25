31f97f54a6b1a9a08c6786259db1dba6818028b0
Patrick Lavin <plavin3@gatech.edu>
2018-09-25 18:17:26 -0400

    update roofline

--- tests/sg-sparse-roofline.sh
+++ tests/sg-sparse-roofline.sh
@@ -3,17 +3,19 @@
 # Create the data for a roofline mode with various sparsities
 
 #Print out each command for debugging purposes
-# set -x
+set -x
 
 #User specified identifier to clarify the output
 DEVICE_NAME=$1
 
 #Specify the vector length - OpenCL allows for specifying double16 for example
 VECTOR="1 2 4 8 16 32 64"
+#Specify the sparsities you want to test for 
+SPARSITY="1 2 4 8 16 32 64 128"
 #Specify the backend - openmp, cuda, opencl
 BACKEND=$2
 
-SCRIPTNAME=sg_roofline
+SCRIPTNAME=sg_sparse_roofline
 
 O_S=${SCRIPTNAME}_${BACKEND}_${DEVICE_NAME}_SCATTER.ssv
 O_G=${SCRIPTNAME}_${BACKEND}_${DEVICE_NAME}_GATHER.ssv
@@ -25,13 +27,16 @@ LEN=$((2**20))
 for V in $VECTOR;
 do
 
-    if [ "${BACKEND}" == "cuda" ]
-    then
-        ./sgbench --backend=cuda --l $LEN -k scatter -v $V --nph -q &>> $O_S
-        ./sgbench --backend=cuda --l $LEN -k gather  -v $V --nph -q &>> $O_G
-        ./sgbench --backend=cuda --l $LEN -k sg      -v $V --nph -q &>> $O_SG
-    else
-        echo "Only CUDA is supported for this script."
-    fi
+    for S in $SPARSITY;
+    do
+        if [ "${BACKEND}" == "cuda" ]
+        then
+            ./sgbench -l $LEN -s $S -k scatter -v $V --nph -q 1>> $O_S
+            ./sgbench -l $LEN -s $S -k gather  -v $V --nph -q 1>> $O_G
+            ./sgbench -l $LEN -s $S -k sg      -v $V --nph -q 1>> $O_SG
+        else
+            echo "Only CUDA is supported for this script."
+        fi
+    done
 
 done
