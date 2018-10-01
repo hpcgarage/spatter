61481f7349feb9f45e92584cf68b8ce154d1a21b
Patrick Lavin <plavin3@gatech.edu>
2018-10-01 11:50:24 -0400

    added opencl  support to roofline script

--- tests/sg-sparse-roofline.sh
+++ tests/sg-sparse-roofline.sh
@@ -3,26 +3,60 @@
 # Create the data for a roofline mode with various sparsities
 
 #Print out each command for debugging purposes
-set -x
+#set -x
+
+SCRIPT='./'`basename "$0"`
+USAGE='\nUsage: \n  '$SCRIPT' openmmp\n  '$SCRIPT' cuda <device>\n  '$SCRIPT' opencl <device> <platform>\n\n'
+
+# Check arguments to script.
+if [ $# -lt 1 ]; then
+    echo -ne $USAGE
+    exit
+fi
+
+#User specifies backend
+BACKEND=$1
+
 
 #User specified identifier to clarify the output
-DEVICE_NAME=$1
+if [ $BACKEND == "cuda" ]; 
+then
+    if [ $# -lt 2 ]; then
+        echo -new $USAGE
+        exit
+    fi
+    DEVICE=$2
+fi
+
+if [ $BACKEND == "opencl" ]; 
+then
+    if [ $# -lt 3 ]; then
+        echo -ne $USAGE
+        exit
+    fi
+    DEVICE=$2
+    PLATFORM=$3
+fi
+
+
+echo backend=${BACKEND} device=${DEVICE}, platform=${PLATFORM}
 
 #Specify the vector length - OpenCL allows for specifying double16 for example
 VECTOR="1 2 4 8 16 32 64"
 #Specify the sparsities you want to test for 
 SPARSITY="1 2 4 8 16 32 64 128"
 #Specify the backend - openmp, cuda, opencl
-BACKEND=$2
 
 SCRIPTNAME=sg_sparse_roofline
 
-O_S=${SCRIPTNAME}_${BACKEND}_${DEVICE_NAME}_SCATTER.ssv
-O_G=${SCRIPTNAME}_${BACKEND}_${DEVICE_NAME}_GATHER.ssv
-O_SG=${SCRIPTNAME}_${BACKEND}_${DEVICE_NAME}_SG.ssv
+O_S=${SCRIPTNAME}_${BACKEND}_${DEVICE}_SCATTER.ssv
+O_G=${SCRIPTNAME}_${BACKEND}_${DEVICE}_GATHER.ssv
+O_SG=${SCRIPTNAME}_${BACKEND}_${DEVICE}_SG.ssv
 
 #Specify a large region to be used for the "sparse space
 LEN=$((2**20))
+
+export CL_HELPER_NO_COMPILER_OUTPUT_NAG=1
 
 for V in $VECTOR;
 do
@@ -34,7 +68,12 @@ do
             ./sgbench -l $LEN -s $S -k scatter -v $V --nph -q 1>> $O_S
             ./sgbench -l $LEN -s $S -k gather  -v $V --nph -q 1>> $O_G
             ./sgbench -l $LEN -s $S -k sg      -v $V --nph -q 1>> $O_SG
-        else
+        elif [ "${BACKEND}" == "opencl" ]
+        then
+            ./sgbench -p $PLATFORM -d $DEVICE -l $LEN -s $S -k scatter -v $V --nph -q 1>> $O_S
+            ./sgbench -p $PLATFORM -d $DEVICE -l $LEN -s $S -k gather  -v $V --nph -q 1>> $O_G
+            ./sgbench -p $PLATFORM -d $DEVICE -l $LEN -s $S -k sg      -v $V --nph -q 1>> $O_SG
+        else 
             echo "Only CUDA is supported for this script."
         fi
     done
