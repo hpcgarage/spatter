62f3dd3ddc6af4477eb7b9885feebab0a0ca0001
Patrick Lavin <plavin3@gatech.edu>
2018-10-02 15:41:40 -0400

    add some static assertions about type sizes

--- include/sgtype.h
+++ include/sgtype.h
@@ -5,23 +5,17 @@
 
 #ifndef SGTYPE_H
 #define SGTYPE_H
+#include <assert.h>
 
-//defines size_t
-//#include <stddef.h>
-
-//TODO: static_assert(sizeof(cl_ulong) == sizeof(sgIdx_t);
-//TODO: static_assert(sizeof(cl_double) == sizeof(sgData_t);
+#ifdef USE_OPENCL
+#include "cl-helper.h"
+static_assert(sizeof(cl_ulong) == sizeof(unsigned long), "Due to size differences between cl_ulong and unsigned long, we cannot compile with OpenCL support on your system");
+static_assert(sizeof(cl_double) == sizeof(double), "Due to size differences between cl_double and double, we cannot compile with OpenCL support on your system");
+static_assert(sizeof(cl_uint) == sizeof(unsigned int), "Due to size differences between cl_uint and unsigned int, we cannot compile with OpenCL support on your system");
+static_assert(sizeof(cl_float) == sizeof(float), "Due to size differences between cl_double and double, we cannot compile with OpenCL support on your system");
+#endif
 
 typedef double sgData_t;
 typedef unsigned long sgIdx_t;
-
-//#include <CL/cl.h>
-//#define SGTYPE_C  cl_double  /**< The OpenCL API type used in C/C++ programs*/
-//#define SGTYPE_C  double  /**< The OpenCL API type used in C/C++ programs*/
-//#define SGTYPE_CL double    /**< The kernel data type corresponding to SGTYPE_C*/
-
-//#ifndef USE_OPENCL
-//	typedef unsigned long cl_ulong;
-//#endif
 
 #endif //endif SGTYPE
