04b79b392d8717d8488c0ca839d94d7919791fb2
Patrick Lavin <plavin3@gatech.edu>
2018-10-04 23:28:40 -0400

    finish volkov script

--- tests/sg-volkov.sh
+++ tests/sg-volkov.sh
@@ -20,13 +20,14 @@ DEVICE=$1
 
 echo backend=${BACKEND} device=${DEVICE}, platform=${PLATFORM}
 
-#Specify the vector length - OpenCL allows for specifying double16 for example
-VECTOR="1 2 4 8 16 32 64"
+
 #Specify the sparsities you want to test for 
 SPARSITY="1 2 4 8 16 32 64 128"
 #Specify the backend - openmp, cuda, opencl
 
 BLOCK="1 2 4 8 16"
+
+SHMEM=`seq 0 15 | tac`
 
 SCRIPTNAME=sg_volkov
 
@@ -35,7 +36,7 @@ O_G=${SCRIPTNAME}_${BACKEND}_${DEVICE}_GATHER.ssv
 O_SG=${SCRIPTNAME}_${BACKEND}_${DEVICE}_SG.ssv
 
 NVPLOG=log.txt
-NVPOPT='--log-file '$NVPLOG' --csv --metrics achieved_occupancy'
+NVPOPT='--aggregate-mode off --log-file '$NVPLOG' --csv --metrics achieved_occupancy'
 WHICH=`seq -s',' 15 3 42`
 
 #Specify a large region to be used for the "sparse space
@@ -45,22 +46,26 @@ for S in $SPARSITY;
 do
     for B in $BLOCK;
     do
-        nvprof $NVPOPT ./sgbench -l $LEN -s $S -k scatter -nph -q -z $B > temp1.ssv
-        cat log.txt | cut -sd',' -f$WHICH | sed '/^$/d' | tr ',' '\n' > temp2.txt
-        paste temp1.ssv temp2.txt >> $O_S
+        for logM in $SHMEM;
+        do
+            M=$((2**logM))
+            nvprof $NVPOPT ./sgbench -l$LEN -s$S -k scatter -nph -q -v16 -z$B -m$M > temp1.ssv
+            cat log.txt | cut -sd',' -f$WHICH | sed '/^$/d' | tr ',' '\n' > temp2.txt
+            paste -d' ' temp1.ssv temp2.txt >> $O_S
 
-        nvprof $NVPOPT ./sgbench -l $LEN -s $S -k gather  -nph -q -z $B > temp1.ssv
-        cat log.txt | cut -sd',' -f$WHICH | sed '/^$/d' | tr ',' '\n' > temp2.txt
-        paste temp1.ssv temp2.txt >> $O_G
+            nvprof $NVPOPT ./sgbench -l$LEN -s$S -k gather  -nph -q -v16 -z$B -m$M > temp1.ssv
+            cat log.txt | cut -sd',' -f$WHICH | sed '/^$/d' | tr ',' '\n' > temp2.txt
+            paste -d' ' temp1.ssv temp2.txt >> $O_G
 
-        nvprof $NVPOPT ./sgbench -l $LEN -s $S -k sg      -nph -q -z $B > temp1.ssv
-        cat log.txt | cut -sd',' -f$WHICH | sed '/^$/d' | tr ',' '\n' > temp2.txt
-        paste temp1.ssv temp2.txt >> $O_SG
+            nvprof $NVPOPT ./sgbench -l$LEN -s$S -k sg      -nph -q -v16 -z$B -m$M > temp1.ssv
+            cat log.txt | cut -sd',' -f$WHICH | sed '/^$/d' | tr ',' '\n' > temp2.txt
+            paste -d' ' temp1.ssv temp2.txt >> $O_SG
+        done
     done
 done
 
 rm $NVPLOG temp1.ssv temp2.txt
 
-# cat log.txt | cut -sd',' -f$WHICH | sed '/^$/d' | tr ',' '\n'
+# cat log.txt | cut -sd',' -m$WHICH | sed '/^$/d' | tr ',' '\n'
 
 # nvprof --log-file log.txt --csv --metrics achieved_occupancy ./sgbench -l $((2**20)) -m 1 -q -nph -z 1 -v16 -s 2 -q -nph  >> testout.txt
