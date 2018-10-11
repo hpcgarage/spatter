fca0b4990fa6d035a4f8f6b66160d04493782ba0
Jeff Young <jyoung9@gatech.edu>
2018-10-11 15:15:14 -0400

    Added updated plotting script

--- plots/generate_plots.sh
+++ plots/generate_plots.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+# Authors: Jeffrey Young, Patrick Lavin
+# Last Modified: October 11, 2018
 #This script runs the R plotting script on the commandline and places output with the data
+#Ex: ./generate_plots.sh openmp condesa-snb 18158 cpu
 
+#Print out each command for debugging purposes
 set -x
+
+SCRIPT='./'`basename "$0"`
+USAGE='\nUsage: \n  '$SCRIPT' <BACKEND=openmp/cuda/opencl> <SYSTEM_DESCRIPTION> <STREAMBW=Stream bandwidth MB/s> <DEVICE=cpu/gpu/knl> \n  Ex: '$SCRIPT' cuda octane-k40 120000 gpu\n\n'
+
+# Check arguments to script.
+if [ $# -lt 4 ]; then
+    echo -ne $USAGE
+    exit
+fi
 
 #Backend language
 BACKEND=$1
@@ -9,19 +22,30 @@ BACKEND=$1
 SYS=$2
 #Stream bandwith
 BW=$3
+#Is the device a CPU, GPU, or KNL?
+DEV=$4
 
 RESULTDIR=../results/v0.2
-SYSDESC=${BACKEND}/cpu/${SYS}
+
+SYSDESC=${BACKEND}/${DEV}/${SYS}
 
 OPERATIONS="scatter gather sg"
 
 for OP in $OPERATIONS; 
 do
 
-#OPUPPER=${OP^^}
+OPUPPER=${OP^^}
 FULLRESULT=${RESULTDIR}/${OP}/${SYSDESC}
 
-echo $FULLRESULT
+FILES="sparse rdm"
+for F in $FILES
+do
+	FILENAME=sg_${F}_roofline_${BACKEND}_${SYS}_${OPUPPER}.ssv
 
-Rscript --vanilla roofline.R  ${FULLRESULT}/*.ssv  ${BACKEND} ${SYS} 0 ${BW} ${FULLRESULT}/${SYS}_${OP}_v02.png
+	echo $FULLRESULT
+	echo $FILENAME
+
+	Rscript --vanilla roofline.R  ${FULLRESULT}/${FILENAME}  ${BACKEND} ${SYS} 0 ${BW} ${FULLRESULT}/${SYS}_${OP}_${F}_v02.png
+
+	done
 done
