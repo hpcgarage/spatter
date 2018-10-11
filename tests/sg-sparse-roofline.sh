15391ac80541fe67870001b92a6271655669caf8
Jeffrey Young <jyoung9@gatech.edu>
2018-10-10 22:39:16 -0400

    Updated roofline script

--- tests/sg-sparse-roofline.sh
+++ tests/sg-sparse-roofline.sh
@@ -46,7 +46,7 @@ VECTOR="1 2 4 8 16 32 64"
 #Specify the sparsities you want to test for 
 SPARSITY="1 2 4 8 16 32 64 128"
 #Note that this number of threads is specific to TX2, Cavium
-NUMTHREADS="14 28 56 112"
+#NUMTHREADS="1 16 32 64"
 
 BLOCK="1 2 4 8 16"
 
@@ -55,6 +55,9 @@ SCRIPTNAME=sg_sparse_roofline
 O_S=${SCRIPTNAME}_${BACKEND}_${DEVICE}_SCATTER.ssv
 O_G=${SCRIPTNAME}_${BACKEND}_${DEVICE}_GATHER.ssv
 O_SG=${SCRIPTNAME}_${BACKEND}_${DEVICE}_SG.ssv
+
+#Use numactl to allocate memory locally and only use one socket
+NUMACTL="numactl -N 0 -l"
 
 #Specify a large region to be used for the "sparse space
 LEN=$((2**20))
@@ -84,17 +87,21 @@ do
             done
         elif [ "${BACKEND}" == "openmp" ]
         then
-	    export OMP_PROC_BIND=spread
-    	    for N in $NUMTHREADS;
-	    do
-	       export OMP_NUM_THREADS=$N
+	    #Attempt to reinforce socket binding with OpenMP if numactl is not available
+	    export OMP_PROC_BIND=true
+	    export OMP_PLACES=sockets
+   	    export OMP_DISPLAY_ENV=VERBOSE
+    	    
+	    #for N in $NUMTHREADS;
+	    #do
+	      # export OMP_NUM_THREADS=$N
            
-	       export OMP_SCHEDULE="static,"$V
-               echo $OMP_SCHEDULE
-               ./sgbench -l $LEN -s $S -k scatter -v $V --nph -q 1>> $O_S
-               ./sgbench -l $LEN -s $S -k gather  -v $V --nph -q 1>> $O_G
-               ./sgbench -l $LEN -s $S -k sg      -v $V --nph -q 1>> $O_SG
-	    done
+	       #export OMP_SCHEDULE="static,"$V
+               #echo $OMP_SCHEDULE
+               $NUMACTL ./sgbench -l $LEN -s $S -k scatter -v $V --nph -q 1>> $O_S
+               $NUMACTL ./sgbench -l $LEN -s $S -k gather  -v $V --nph -q 1>> $O_G
+               $NUMACTL ./sgbench -l $LEN -s $S -k sg      -v $V --nph -q 1>> $O_SG
+	    #done
         else 
             echo "Unknown backend" 
         fi
