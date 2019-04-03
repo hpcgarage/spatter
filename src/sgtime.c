338eaf45efed66d18c50f507aae76464e1d726e7
Jeff Young <jyoung9@gatech.edu>
2019-04-02 19:32:39 -0700

    Removed debug print statements

--- src/sgtime.c
+++ src/sgtime.c
@@ -18,8 +18,9 @@ double diff_ms(void)
 if ((endtime.tv_nsec-starttime.tv_nsec)<0) {
 	diff_s = endtime.tv_sec-(starttime.tv_sec+1);
 	diff_ns = (1000000000+endtime.tv_nsec)-starttime.tv_nsec;
-  	printf("Time is now %llu s and %llu ns for endtime and %llu s and %llu ns for t\n", endtime.tv_sec, endtime.tv_nsec, starttime.tv_sec, starttime.tv_nsec);
- 	printf("diff_s is %llu and diff_ns is %llu \n",diff_s,diff_ns);
+  	//Debugging
+	//printf("Time is now %llu s and %llu ns for endtime and %llu s and %llu ns for t\n", endtime.tv_sec, endtime.tv_nsec, starttime.tv_sec, starttime.tv_nsec);
+ 	//printf("diff_s is %llu and diff_ns is %llu \n",diff_s,diff_ns);
 } else {
 	diff_s = endtime.tv_sec - starttime.tv_sec;
  	diff_ns = endtime.tv_nsec - starttime.tv_nsec;
@@ -28,7 +29,7 @@ if ((endtime.tv_nsec-starttime.tv_nsec)<0) {
  diff_ms = ((double)diff_s * 1000.0) + ((double)diff_ns / 1000000.0);
  
  //Print out for debugging
- printf("The difference in ms is %f\n",diff_ms);
+ //printf("The difference in ms is %f\n",diff_ms);
  
  return diff_ms;
 
