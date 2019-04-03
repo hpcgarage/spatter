1f138595cf23dea74b9db2ba5875227ce7c8bbc6
Jeff Young <jyoung9@gatech.edu>
2019-04-02 19:28:45 -0700

    Updated timer to fix overflow case

--- src/sgtime.c
+++ src/sgtime.c
@@ -13,12 +13,22 @@ double diff_ms(void)
 {
  unsigned long long diff_ns, diff_s; 
  double diff_ms;
- diff_ns = endtime.tv_nsec - starttime.tv_nsec;
- diff_s = endtime.tv_sec - starttime.tv_sec;
+ struct timespec temp;
+
+if ((endtime.tv_nsec-starttime.tv_nsec)<0) {
+	diff_s = endtime.tv_sec-(starttime.tv_sec+1);
+	diff_ns = (1000000000+endtime.tv_nsec)-starttime.tv_nsec;
+  	printf("Time is now %llu s and %llu ns for endtime and %llu s and %llu ns for t\n", endtime.tv_sec, endtime.tv_nsec, starttime.tv_sec, starttime.tv_nsec);
+ 	printf("diff_s is %llu and diff_ns is %llu \n",diff_s,diff_ns);
+} else {
+	diff_s = endtime.tv_sec - starttime.tv_sec;
+ 	diff_ns = endtime.tv_nsec - starttime.tv_nsec;
+}
+
  diff_ms = ((double)diff_s * 1000.0) + ((double)diff_ns / 1000000.0);
  
  //Print out for debugging
- //printf("The difference in ms is %f\n",diff_ms);
+ printf("The difference in ms is %f\n",diff_ms);
  
  return diff_ms;
 
