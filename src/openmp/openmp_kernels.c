45dc713bc0d229d8b05600c58f083e1c7e045ce1
Patrick Lavin <plavin3@gatech.edu>
2019-12-06 14:51:21 -0500

    remove white space

--- src/openmp/openmp_kernels.c
+++ src/openmp/openmp_kernels.c
@@ -6,7 +6,7 @@
 
 
 void sg_omp(
-            sgData_t* restrict target,   
+            sgData_t* restrict target,
             sgIdx_t*     restrict ti,
             sgData_t* restrict source,
             sgIdx_t*     restrict si,
@@ -20,7 +20,7 @@ void sg_omp(
 }
 
 void scatter_omp(
-            sgData_t* restrict target, 
+            sgData_t* restrict target,
             sgIdx_t*     restrict ti,
             sgData_t* restrict source,
             sgIdx_t*     restrict si,
@@ -36,7 +36,7 @@ void scatter_omp(
 }
 
 void gather_omp(
-            sgData_t* restrict target, 
+            sgData_t* restrict target,
             sgIdx_t*     restrict ti,
             sgData_t* restrict source,
             sgIdx_t*     restrict si,
@@ -56,11 +56,11 @@ int get_ind(void) {
 }
 
 void gather(
-		sgData_t* restrict target, 
-		sgData_t* restrict source, 
-		sgIdx_t*  const restrict pat, 
+		sgData_t* restrict target,
+		sgData_t* restrict source,
+		sgIdx_t*  const restrict pat,
 		size_t    pat_len,
-		size_t    delta, 
+		size_t    delta,
 		size_t    n)
 {
 
@@ -77,32 +77,32 @@ void gather(
 }
 
 void gather_smallbuf(
-        sgData_t** restrict target, 
-        sgData_t* const restrict source, 
-        sgIdx_t* const restrict pat, 
-        size_t pat_len, 
-        size_t delta, 
-        size_t n, 
+        sgData_t** restrict target,
+        sgData_t* const restrict source,
+        sgIdx_t* const restrict pat,
+        size_t pat_len,
+        size_t delta,
+        size_t n,
         size_t target_len) {
 #ifdef __GNUC__
-    #pragma omp parallel 
-#else 
+    #pragma omp parallel
+#else
     #pragma omp parallel shared(pat)
 #endif
     {
         int t = omp_get_thread_num();
- 
+
 #ifdef __CRAYC__
-    #pragma concurrent 
+    #pragma concurrent
 #endif
 #ifdef __INTEL_COMPILER
     #pragma ivdep
 #endif
 #pragma omp for
         for (size_t i = 0; i < n; i++) {
-           sgData_t *sl = source + delta * i; 
+           sgData_t *sl = source + delta * i;
            sgData_t *tl = target[t] + pat_len*(i%target_len);
-#ifdef __CRAYC__  
+#ifdef __CRAYC__
     #pragma concurrent
 #endif
 #if defined __CRAYC__ || defined __INTEL_COMPILER
@@ -116,32 +116,32 @@ void gather_smallbuf(
 }
 
 void gather_smallbuf_rdm(
-        sgData_t** restrict target, 
-        sgData_t* const restrict source, 
-        sgIdx_t* const restrict pat, 
-        size_t pat_len, 
-        size_t delta, 
-        size_t n, 
+        sgData_t** restrict target,
+        sgData_t* const restrict source,
+        sgIdx_t* const restrict pat,
+        size_t pat_len,
+        size_t delta,
+        size_t n,
         size_t target_len) {
 #ifdef __GNUC__
-    #pragma omp parallel 
-#else 
+    #pragma omp parallel
+#else
     #pragma omp parallel shared(pat)
 #endif
     {
         int t = omp_get_thread_num();
- 
+
 #ifdef __CRAYC__
-    #pragma concurrent 
+    #pragma concurrent
 #endif
 #ifdef __INTEL_COMPILER
     #pragma ivdep
 #endif
 #pragma omp for
         for (size_t i = 0; i < n; i++) {
-           sgData_t *sl = source + rand()%((n-1)*delta); 
+           sgData_t *sl = source + rand()%((n-1)*delta);
            sgData_t *tl = target[t] + pat_len*(i%target_len);
-#ifdef __CRAYC__  
+#ifdef __CRAYC__
     #pragma concurrent
 #endif
 #if defined __CRAYC__ || defined __INTEL_COMPILER
@@ -155,30 +155,30 @@ void gather_smallbuf_rdm(
 }
 
 void scatter_smallbuf(
-        sgData_t* restrict target, 
-        sgData_t** const restrict source, 
-        sgIdx_t* const restrict pat, 
-        size_t pat_len, 
-        size_t delta, 
-        size_t n, 
+        sgData_t* restrict target,
+        sgData_t** const restrict source,
+        sgIdx_t* const restrict pat,
+        size_t pat_len,
+        size_t delta,
+        size_t n,
         size_t source_len) {
 #ifdef __GNUC__
-    #pragma omp parallel 
-#else 
+    #pragma omp parallel
+#else
     #pragma omp parallel shared(pat)
 #endif
     {
         int t = omp_get_thread_num();
- 
+
 #ifdef __CRAYC__
-    #pragma concurrent 
+    #pragma concurrent
 #endif
 #ifdef __INTEL_COMPILER
     #pragma ivdep
 #endif
 #pragma omp for
         for (size_t i = 0; i < n; i++) {
-           sgData_t *tl = target + delta * i; 
+           sgData_t *tl = target + delta * i;
            sgData_t *sl = source[t] + pat_len*(i%source_len);
 #ifdef __CRAYC__
     #pragma concurrent
@@ -194,17 +194,17 @@ void scatter_smallbuf(
 }
 
 void gather_smallbuf_random(
-        sgData_t** restrict target, 
-        sgData_t* const restrict source, 
-        sgIdx_t* const restrict pat, 
-        size_t pat_len, 
-        size_t delta, 
-        size_t n, 
-        size_t target_len, 
+        sgData_t** restrict target,
+        sgData_t* const restrict source,
+        sgIdx_t* const restrict pat,
+        size_t pat_len,
+        size_t delta,
+        size_t n,
+        size_t target_len,
         long initstate) {
 #ifdef __GNUC__
-    #pragma omp parallel 
-#else 
+    #pragma omp parallel
+#else
     #pragma omp parallel shared(pat)
 #endif
     {
@@ -212,15 +212,15 @@ void gather_smallbuf_random(
         pcg32_random_t rng;
         pcg32_srandom_r(&rng, initstate, t);
 
- 
+
 #ifdef __CRAYC__
-    #pragma concurrent 
+    #pragma concurrent
 #endif
 #pragma omp for
         for (size_t i = 0; i < n; i++) {
             //long r = ()%n;
            uint32_t r = pcg32_boundedrand_r(&rng, (uint32_t)n);
-           sgData_t *sl = source + delta * r; 
+           sgData_t *sl = source + delta * r;
            sgData_t *tl = target[t] + pat_len*(i%target_len);
 #ifdef __CRAYC__
     #pragma concurrent
@@ -234,32 +234,32 @@ void gather_smallbuf_random(
 }
 
 void scatter_smallbuf_random(
-        sgData_t* restrict target, 
-        sgData_t** const restrict source, 
-        sgIdx_t* const restrict pat, 
-        size_t pat_len, 
-        size_t delta, 
-        size_t n, 
+        sgData_t* restrict target,
+        sgData_t** const restrict source,
+        sgIdx_t* const restrict pat,
+        size_t pat_len,
+        size_t delta,
+        size_t n,
         size_t source_len,
         long initstate) {
     if (n > 1ll<<32) {printf("n too big for rng, exiting.\n"); exit(1);}
 #ifdef __GNUC__
-    #pragma omp parallel 
-#else 
+    #pragma omp parallel
+#else
     #pragma omp parallel shared(pat)
 #endif
     {
         int t = omp_get_thread_num();
         pcg32_random_t rng;
         pcg32_srandom_r(&rng, initstate, t);
- 
+
 #ifdef __CRAYC__
-    #pragma concurrent 
+    #pragma concurrent
 #endif
 #pragma omp for
         for (size_t i = 0; i < n; i++) {
            uint32_t r = pcg32_boundedrand_r(&rng, (uint32_t)n);
-           sgData_t *tl = target + delta * r; 
+           sgData_t *tl = target + delta * r;
            sgData_t *sl = source[t] + pat_len*(i%source_len);
 #ifdef __CRAYC__
     #pragma concurrent
@@ -274,28 +274,28 @@ void scatter_smallbuf_random(
 
 
 void gather_smallbuf_multidelta(
-        sgData_t** restrict target, 
-        sgData_t* restrict source, 
-        sgIdx_t* const restrict pat, 
-        size_t pat_len, 
-        size_t *delta, 
-        size_t n, 
+        sgData_t** restrict target,
+        sgData_t* restrict source,
+        sgIdx_t* const restrict pat,
+        size_t pat_len,
+        size_t *delta,
+        size_t n,
         size_t target_len,
         size_t delta_len) {
 #ifdef __GNUC__
-    #pragma omp parallel 
-#else 
+    #pragma omp parallel
+#else
     #pragma omp parallel shared(pat)
 #endif
     {
         int t = omp_get_thread_num();
 #ifdef __CRAYC__
-    #pragma concurrent 
+    #pragma concurrent
 #endif
         //taget_len is in multiples of pat_len
 #pragma omp for
         for (size_t i = 0; i < n; i++) {
-           sgData_t *sl = source + (i/delta_len)*delta[delta_len-1] + delta[i%delta_len] - delta[0]; 
+           sgData_t *sl = source + (i/delta_len)*delta[delta_len-1] + delta[i%delta_len] - delta[0];
            sgData_t *tl = target[t] + pat_len*(i%target_len);
            //sgData_t *sl = source;
            //sgData_t *tl = target[0];
@@ -311,13 +311,13 @@ void gather_smallbuf_multidelta(
         }
     }
 }
-		
+
 void scatter(
-		sgData_t* restrict target, 
-		sgData_t* restrict source, 
-		sgIdx_t*  const restrict pat, 
+		sgData_t* restrict target,
+		sgData_t* restrict source,
+		sgIdx_t*  const restrict pat,
 		size_t    pat_len,
-		size_t    delta, 
+		size_t    delta,
 		size_t    n)
 {
 
@@ -333,12 +333,12 @@ void scatter(
     }
 }
 void gather_stride_os(
-		sgData_t* restrict target, 
-		sgData_t* restrict source, 
-		sgIdx_t*  restrict pat, 
+		sgData_t* restrict target,
+		sgData_t* restrict source,
+		sgIdx_t*  restrict pat,
 		size_t    pat_len,
-		size_t    delta, 
-		size_t    n, 
+		size_t    delta,
+		size_t    n,
         size_t    target_wrap)
 {
     for (size_t i = 0; i < n; i++) {
@@ -348,29 +348,29 @@ void gather_stride_os(
         source += delta;
     }
 }
-		
-		
+
+
 void gather_stride8(
-		sgData_t* restrict target, 
-		sgData_t* restrict source, 
-		sgIdx_t*  restrict pat, 
-		size_t    delta, 
+		sgData_t* restrict target,
+		sgData_t* restrict source,
+		sgIdx_t*  restrict pat,
+		size_t    delta,
 		size_t    n)
 {
     for (size_t i = 0; i < n; i++) {
         for (size_t j = 0; j < 8; j++) {
             target[i*8+j] = source[pat[j]];
         }
-        source += delta;    
+        source += delta;
     }
 }
 
 void gather_stride16(
-		sgData_t* restrict target, 
-		sgData_t* restrict source, 
-		sgIdx_t*  restrict pat, 
+		sgData_t* restrict target,
+		sgData_t* restrict source,
+		sgIdx_t*  restrict pat,
         size_t    stride,
-		size_t    delta, 
+		size_t    delta,
 		size_t    n)
 {
     for (size_t i = 0; i < n; i++) {
@@ -378,12 +378,12 @@ void gather_stride16(
             //printf("%zu <- %zu\n", i*16+j, pat[j]);
             target[i*16+j] = source[pat[j]];
         }
-        source += delta;    
+        source += delta;
     }
 }
 
 void sg_accum_omp(
-            sgData_t* restrict target, 
+            sgData_t* restrict target,
             sgIdx_t*     restrict ti,
             sgData_t* restrict source,
             sgIdx_t*     restrict si,
@@ -397,7 +397,7 @@ void sg_accum_omp(
 }
 
 void scatter_accum_omp(
-            sgData_t* restrict target, 
+            sgData_t* restrict target,
             sgIdx_t*     restrict ti,
             sgData_t* restrict source,
             sgIdx_t*     restrict si,
@@ -410,7 +410,7 @@ void scatter_accum_omp(
 }
 
 void gather_accum_omp(
-            sgData_t* restrict target, 
+            sgData_t* restrict target,
             sgIdx_t*     restrict ti,
             sgData_t* restrict source,
             sgIdx_t*     restrict si,
