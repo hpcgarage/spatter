4199f8547306a11547c20626d1994aa18377e2a0
James Wood <woodjamesdee@gmail.com>
2020-08-05 09:03:54 -0400

    Fix for gather kernel on FPGA.

--- src/sycl/sycl_backend.hpp
+++ src/sycl/sycl_backend.hpp
@@ -3,6 +3,6 @@
 
 #include "../../include/sgtype.h"
 
-double sycl_gather(double* src, size_t src_size, sgIdx_t* idx, size_t idx_len, size_t delta, unsigned long global_work_size, unsigned long local_work_size);
+double sycl_gather(double* src, size_t src_size, sgIdx_t* idx, size_t idx_len, size_t delta, unsigned int* grid, unsigned int* block, unsigned int dim);
 
 #endif
