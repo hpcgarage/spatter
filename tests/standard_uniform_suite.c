9f724e14a2e42f3731cbd014f2e900f470650bff
agustin-vaca <agustin.vaca@outlook.com>
2022-11-06 22:18:33 -0500

    Reformatted standard_uniform_suite.c

--- tests/standard_uniform_suite.c
+++ tests/standard_uniform_suite.c
@@ -3,24 +3,28 @@
 int uniform_test_length_gap() {
     int length = 4, gap = 2;
     for (int i = 0; i < 10; i++) {
-        const char *command = sprintf("./spatter -pUNIFORM:%d:%d", length, gap);
-        if (system(command) == EXIT_FAILURE) {
+        char *command;
+        int ret = asprintf(&command, "./spatter -pUNIFORM:%d:%d", length, gap);
+        if (ret == -1 || system(command) == EXIT_FAILURE) {
             printf("Test failure on %s", command);
             return EXIT_FAILURE;
         }
         length *= 2;
         gap *= 2;
+        free(command);
     }
     return EXIT_SUCCESS;
 }
 
 int uniform_test_delta() {
-    for (int delta = 1; delta < 100; delta*=2) {
-        const char *command = sprintf("./spatter -pUNIFORM:8:4 -d%d", delta);
-        if (system(command) == EXIT_FAILURE) {
+    for (int delta = 1; delta < 100; delta *= 2) {
+        const char *command;
+        int ret = asprintf(&command, "./spatter -pUNIFORM:8:4 -d%d", delta);
+        if (ret == -1 || system(command) == EXIT_FAILURE) {
             printf("Test failure on %s", command);
             return EXIT_FAILURE;
         }
+        free(command);
     }
     return EXIT_SUCCESS;
 }
