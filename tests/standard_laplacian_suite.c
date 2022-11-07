59cb8585cd757544c3caf36d3898f9f8fa030e43
agustin-vaca <agustin.vaca@outlook.com>
2022-11-06 22:23:54 -0500

    Reformatted to use asprintf

--- tests/standard_laplacian_suite.c
+++ tests/standard_laplacian_suite.c
@@ -3,22 +3,25 @@
 int laplacian_test() {
     int dimension = 1, pseudo_order = 1, problem_size = 100;
     for (int i = 0; i < 10; i++) {
-        const char *command = sprintf("./spatter -pLAPLACIAN:%d:%d:%d", dimension, pseudo_order, problem_size);
-        if (system(command) == EXIT_FAILURE) {
+        const char *command;
+        int ret = asprintf(&command, "./spatter -pLAPLACIAN:%d:%d:%d", dimension, pseudo_order, problem_size);
+        if (ret == -1 || system(command) == EXIT_FAILURE) {
             printf("Test failure on %s", command);
             return EXIT_FAILURE;
         }
         dimension *= 2;
         pseudo_order *= 2;
         problem_size += 100;
+        free(command);
     }
     return EXIT_SUCCESS;
 }
 
 int laplacian_test_delta() {
-    for (int delta = 1; delta < 100; delta*=2) {
-        const char *command = sprintf("./spatter -pLAPLACIAN:2:2:100 -d%d", delta);
-        if (system(command) == EXIT_FAILURE) {
+    for (int delta = 1; delta < 100; delta *= 2) {
+        const char *command;
+        int ret = asprintf(&command, "./spatter -pLAPLACIAN:2:2:100 -d%d", delta);
+        if (ret == -1 || system(command) == EXIT_FAILURE) {
             printf("Test failure on %s", command);
             return EXIT_FAILURE;
         }
