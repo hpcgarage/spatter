cae8ef3c5c15444582f31350c156b19c07d333e7
Patrick Lavin <plavin997@gmail.com>
2022-11-12 09:52:38 -0500

    add tests for new kernel

---
+++ tests/parse_concurrent.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "parse-args.h"
+
+#define STRLEN (1024)
+
+int main (int argc, char **argv)
+{
+    int nrc = 0;
+    struct run_config *rc;
+
+    int argc_ = 4;
+    char **argv_ = (char**)malloc(sizeof(char*) * argc_);
+    for (int i = 0; i < argc_; i++) {
+        argv_[i] = (char*)malloc(sizeof(char)*STRLEN);
+    }
+
+    strcpy(argv_[0], "./spatter");
+    strcpy(argv_[1], "-hUNIFORM:8:1");
+    strcpy(argv_[2], "-gUNIFORM:8:1");
+    strcpy(argv_[3], "-kSG");
+
+    parse_args(argc_, argv_, &nrc, &rc);
+
+    if (nrc != 1)
+        printf("Test failure on Uniform Stride: Expected number of runs to be 1, actually was %d.\n", nrc);
+
+    if (rc == NULL)
+        printf("Test failure on Uniform Stride: failed to create or allocate run_config struct.\n");
+
+    int gold[8] = {0,1,2,3,4,5,6,7};
+
+    for (int i = 0; i < 8; i++) {
+        if (gold[i] != rc[0].pattern_scatter[i]) {
+            return EXIT_FAILURE;
+        }
+        if (gold[i] != rc[0].pattern_gather[i]) {
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
+
+}
