fed8482edfdde6209f82760485075d33288d1a98
agustin-vaca <agustin.vaca@outlook.com>
2022-11-21 14:53:06 -0500

    Added lulesh test to tests/standard_suite.c

--- tests/standard_suite.c
+++ tests/standard_suite.c
@@ -23,11 +23,25 @@ int cpu_ustride_test() {
     return EXIT_SUCCESS;
 }
 
+int cpu_lulesh_test() {
+    char *command;
+    int ret = asprintf(&command, "../spatter -pFILE=../../standard-suite/app-traces/lulesh.json");
+        if (ret == -1 || system(command) != EXIT_SUCCESS) {
+            printf("Test failure on %s", command);
+            return EXIT_FAILURE;
+        }
+        free(command);
+        return EXIT_SUCCESS;
+}
+
 int main(int argc, char **argv) {
     if (cpu_stream_test() != EXIT_SUCCESS) {
         return EXIT_FAILURE;
     }
     if (cpu_ustride_test() != EXIT_SUCCESS) {
+        return EXIT_FAILURE;
+    }
+    if (cpu_lulesh_test() != EXIT_SUCCESS) {
         return EXIT_FAILURE;
     }
 
