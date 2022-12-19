5634a3e3c9d9271c49148699208393ce93b7d2e2
Jered Dominguez-Trujillo <jereddt@lanl.gov>
2022-12-19 15:05:40 -0700

    Fixed multilevel test and re-ran portion of Jupyter notebook

--- tests/multilevel.c
+++ tests/multilevel.c
@@ -4,7 +4,7 @@
 int uniform_multilevel_test() {
     for (int i = 1; i <= 10; i++) {
         char *command;
-        int ret = asprintf(&command, "../spatter -kMultiGather -pUNIFORM:%d:%d -gUNIFORM:%d:%d ", i, i, i, i);
+        int ret = asprintf(&command, "../spatter -kMultiGather -pUNIFORM:%d:%d -gUNIFORM:%d:%d ", i * i, i, i, i);
         if (ret == -1 || system(command) != EXIT_SUCCESS) {
             printf("Test failure on %s", command);
             return EXIT_FAILURE;
@@ -13,7 +13,7 @@ int uniform_multilevel_test() {
     }
     for (int i = 1; i <= 10; i++) {
         char *command;
-        int ret = asprintf(&command, "../spatter -kMultiScatter -pUNIFORM:%d:%d -hUNIFORM:%d:%d ", i, i, i, i);
+        int ret = asprintf(&command, "../spatter -kMultiScatter -pUNIFORM:%d:%d -hUNIFORM:%d:%d ", i * i, i, i, i);
         if (ret == -1 || system(command) != EXIT_SUCCESS) {
             printf("Test failure on %s", command);
             return EXIT_FAILURE;
