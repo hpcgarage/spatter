2c8b2dc742a965aa8c79f99922222a46ccf7e378
Jered Dominguez-Trujillo <jereddt@lanl.gov>
2023-12-06 21:47:41 -0700

    number of threads from JSON file

--- src/Spatter/JSONParser.hh
+++ src/Spatter/JSONParser.hh
@@ -13,6 +13,10 @@
 
 #include <nlohmann/json.hpp>
 
+#ifdef USE_OPENMP
+#include <omp.h>
+#endif
+
 #include "Spatter/Configuration.hh"
 #include "Spatter/PatternParser.hh"
 
@@ -22,10 +26,11 @@ namespace Spatter {
 
 class JSONParser {
 public:
-  JSONParser(std::string filename, std::string backend = "serial",
-      std::string kernel = "gather", unsigned long nruns = 10,
-      unsigned long verbosity = 3)
-      : backend_(backend), default_kernel_(kernel), default_nruns_(nruns),
+  JSONParser(std::string filename, const std::string backend = "serial",
+      const std::string kernel = "gather", const int nthreads = 1,
+      const unsigned long nruns = 10, const unsigned long verbosity = 3)
+      : backend_(backend), default_kernel_(kernel),
+        default_omp_threads_(nthreads), default_nruns_(nruns),
         default_verbosity_(verbosity) {
     if (!file_exists_(filename)) {
       std::cerr << "File does not exist" << std::endl;
@@ -49,6 +54,9 @@ public:
         v["kernel"] = kernel;
       }
 
+      if (!v.contains("nthreads"))
+        v["nthreads"] = default_omp_threads_;
+
       if (!v.contains("nruns"))
         v["nruns"] = default_nruns_;
 
@@ -64,6 +72,7 @@ public:
 
     assert(data_[index].contains("kernel"));
     assert(data_[index].contains("pattern"));
+    assert(data_[index].contains("nthreads"));
     assert(data_[index].contains("nruns"));
     assert(data_[index].contains("verbosity"));
 
@@ -118,8 +127,8 @@ public:
 #ifdef USE_OPENMP
     else if (backend_.compare("openmp") == 0)
       c = std::make_unique<Spatter::Configuration<Spatter::OpenMP>>(
-          data_[index]["kernel"], pattern, data_[index]["nruns"],
-          data_[index]["verbosity"]);
+          data_[index]["kernel"], pattern, data_[index]["nthreads"],
+          data_[index]["nruns"], data_[index]["verbosity"]);
 #endif
 #ifdef USE_CUDA
     else if (backend_.compare("cuda") == 0)
@@ -151,9 +160,10 @@ private:
   size_t size_;
   std::string backend_;
 
-  std::string default_kernel_;
-  unsigned long default_nruns_;
-  unsigned long default_verbosity_;
+  const std::string default_kernel_;
+  const int default_omp_threads_;
+  const unsigned long default_nruns_;
+  const unsigned long default_verbosity_;
 };
 
 } // namespace Spatter
